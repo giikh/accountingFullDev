6eb119a80fa78ea1f9c19182462b2b068ef53a7c
Saruulyy <c23io1078@ufe.edu.mn>
2024-04-04 16:28:48 +0800

    test

--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -1,9 +1,22 @@
 <template>
-  <h1>HomePage</h1>
-  <!-- <a http://boyo.mn/salary/> -->
-  <a href="http://boyo.mn/salary/">Salary</a> 
-  <a href="http://boyo.mn/goods/">Goods&Materials</a>
+  <h1 href="http://boyo.mn/HomePage/">HomePage</h1>
+ <h2 href="http://boyo.mn/salary/">Salary</h2> 
+ <h2 href="http://boyo.mn/goods/">Inventory</h2> 
 </template>
+
+<style>
+/* Add your CSS styles here */
+h1, h2 {
+  color: blue;
+}
+h2 {
+  font-size: 1.2em;
+}
+a {
+  text-decoration: none;
+  color: inherit;
+}
+</style>
 
 <script>
 import { defineComponent } from "vue";
