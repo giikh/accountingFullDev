876f93987f644cc1de500f7a91c5c40f3f097aba
Saruulyy <c23io1078@ufe.edu.mn>
2024-04-11 15:46:42 +0800

    CT3

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -12,6 +12,7 @@ import GuilbalA from "./pages/GuilbalA.vue";
 import Ct_1 from "./pages/Ct_1.vue";
 import Hynalt from "./pages/HynaltHu.vue";
 import DansHutlult from "./pages/DansHutlult.vue";
+import Ct_3 from "./pages/Ct_3.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -72,10 +73,16 @@ const router = createRouter({
       meta: { title: "hynalt" },
     },
     {
-      path: "/dans_hutlult",
-      name: "dans_hutlult",
+      path: "/dans_tuluvluguu",
+      name: "dans_tuluvluguu",
       component: DansHutlult,
-      meta: { title: "dans_hutlult" },
+      meta: { title: "dans_tuluvluguu" },
+    },
+    {
+      path: "/Ct_3",
+      name: "Ct_3",
+      component: Ct_3,
+      meta: { title: "Ct_3" },
     },
   ],
   scrollBehavior(to) {
